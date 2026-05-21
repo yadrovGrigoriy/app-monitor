ff7e5aba300c39fdde2d99903b0f326b4070436a
yadrovGrigoriy <anger@211.ru>
2026-05-21 22:29:34 +0700

    chore: remove generator scripts from repo, update .gitignore

--- gen_all.py
+++
@@ -1,7 +0,0 @@
-import os, base64
-base = "C:/Users/Григорий/code/AppMonitor"
-files = {}
-files["core/database.py"] = '# sqlite db\n'
-files['requirements.txt'] = 'PyQt5>=5.15\npywin32>=306\nschedule>=1.2\n'
-files['core/__init__.py'] = ''
-files['ui/__init__.py'] = ''
